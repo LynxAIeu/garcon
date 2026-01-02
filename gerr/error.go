b63824f2f342c7725abd881d75aa6a8d44d1af14
Oliver <oliver@LM4.eu>
2026-01-02 14:54:21 +0100

    feat: add error providing file and line number of the source code

---
+++ gerr/error.go
@@ -0,0 +1,164 @@
+// Copyright 2021 The contributors of Garcon.
+// This file is part of Garcon, a static web builder, API server and middleware using Git, docker and podman.
+// SPDX-License-Identifier: MIT
+
+// Package gerr (Garcon Error) is MCP compliant,
+// by producing the error object as specified in JSON-RPC 2.0:
+// https://www.jsonrpc.org/specification#error_object
+//
+//	code must be a number
+//	 -32768 to -32000  Reserved for pre-defined errors:
+//	 -32700            Parse error, the server received an invalid JSON, or had an issue while parsing the JSON text
+//	 -32603            Internal JSON-RPC error
+//	 -32602            Invalid method parameters
+//	 -32601            Method not found, the method does not exist or is not available
+//	 -32600            Invalid Request, the JSON sent is not a valid Request object
+//	 -32099 to -32000  Implementation-defined server-errors
+//
+//	msg  string providing a short description of the error (one concise single sentence).
+//
+//	data     optional, any type, additional information about the error.
+package gerr
+
+import (
+	"fmt"
+	"runtime"
+	"strconv"
+	"strings"
+	"time"
+)
+
+type (
+	// Error implements the error structure defined in JSON-RPC 2.0.
+	Error struct {
+		Data    Data   `json:"data,omitzero"`
+		Message string `json:"msg,omitempty"`
+		Code    Code   `json:"code,omitempty"`
+	}
+
+	// Data contains the error details.
+	Data struct {
+		Time     time.Time      `json:"time,omitzero"`
+		Cause    error          `json:"cause,omitempty"`
+		Params   map[string]any `json:"params,omitempty"`
+		Function string         `json:"function,omitempty"`
+		FileLine string         `json:"file_line,omitempty"`
+	}
+
+	// Code represents the type of error.
+	Code int64
+)
+
+const (
+	// Invalid indicates validation errors.
+	Invalid Code = iota + -32149
+	// ConfigErr indicates configuration errors.
+	ConfigErr
+	// InferErr indicates inference-related errors.
+	InferErr
+	// UserAbort occurs when /abort is requested.
+	UserAbort
+	// ServerErr indicates server-related errors.
+	ServerErr
+	// Timeout indicates timeout-related errors.
+	Timeout
+	// NotFound indicates resource not found errors.
+	NotFound
+)
+
+// New creates a new gerr.Error.
+func New(code Code, msg string, args ...any) *Error {
+	return wrap(nil, code, msg, args...)
+}
+
+// Wrap an existing error.
+func Wrap(err error, code Code, msg string, args ...any) *Error {
+	return wrap(err, code, msg, args...)
+}
+
+func wrap(cause error, code Code, msg string, args ...any) *Error {
+	err := &Error{
+		Code:    code,
+		Message: msg,
+		Data: Data{
+			Time:  time.Now(),
+			Cause: cause,
+		},
+	}
+
+	var pcs [1]uintptr
+	runtime.Callers(3, pcs[:]) // skip 3 calls in the callstack: [runtime.Callers, wrap, New/Wrap]
+	if pcs[0] != 0 {
+		fs := runtime.CallersFrames([]uintptr{pcs[0]})
+		f, _ := fs.Next()
+		err.Data.Function = f.Function
+		err.Data.FileLine = f.File
+		if f.Line != 0 {
+			err.Data.FileLine += ":" + strconv.Itoa(f.Line)
+		}
+	}
+
+	err.Data.Params = make(map[string]any, (len(args)+1)/2)
+	for len(args) > 0 {
+		var key string
+		var val any
+		key, val, args = getPairRest(args)
+		err.Data.Params[key] = val
+	}
+
+	return err
+}
+
+func getPairRest(args []any) (key string, val any, rest []any) {
+	if len(args) == 1 {
+		return "!BADKEY", args[0], nil
+	}
+	key, ok := args[0].(string)
+	if !ok {
+		key = fmt.Sprint(args[0])
+	}
+	return key, args[1], args[2:]
+}
+
+// Error implements the error interface.
+func (e *Error) Error() string {
+	begin := []byte(" ())))))))))))))))))))))))))))))))))")
+	begin = strconv.AppendInt(begin[:2], int64(e.Code), 10)
+
+	var builder strings.Builder
+	builder.Write(begin[:len(begin)+1])
+
+	for key, val := range e.Data.Params {
+		builder.WriteByte(byte(' '))
+		builder.WriteString(key)
+		builder.WriteByte(byte('='))
+		builder.WriteString(fmt.Sprint(val))
+	}
+
+	if e.Data.Cause != nil {
+		builder.WriteString(" cause: ")
+		builder.WriteString(e.Data.Cause.Error())
+	}
+
+	if e.Data.Function != "" {
+		builder.WriteString(" in ")
+		builder.WriteString(e.Data.Function)
+	}
+
+	if e.Data.FileLine != "" {
+		builder.WriteByte(byte(':'))
+		builder.WriteString(e.Data.FileLine)
+	}
+
+	if !e.Data.Time.IsZero() {
+		builder.WriteByte(byte(' '))
+		builder.WriteString(e.Data.Time.Format("2006-01-02 15:04:05.999"))
+	}
+
+	return builder.String()
+}
+
+// Unwrap returns the underlying error for error unwrapping.
+func (e *Error) Unwrap() error {
+	return e.Data.Cause
+}
---
+++ gerr/http.go
@@ -0,0 +1,39 @@
+// Copyright 2021 The contributors of Garcon.
+// This file is part of Garcon, a static web builder, API server and middleware using Git, docker and podman.
+// SPDX-License-Identifier: MIT
+
+package gerr
+
+import (
+	"errors"
+	"net/http"
+)
+
+// HttpError provides ready to use HTTP code and payload for HTTP handlers.
+func HttpError(err error) (int, error) {
+	// Return a standardized error response
+	var gErr *Error
+	if !errors.As(err, &gErr) {
+		// If not an gerr.Error, wrap it and return internal server error
+		gErr = wrap(err, ServerErr, "internal server error")
+	}
+	return statusCode(gErr.Code), gErr
+}
+
+// statusCode deduce the HTTP status code from an ErrorType.
+func statusCode(errType Code) int {
+	switch errType {
+	case Invalid:
+		return http.StatusBadRequest
+	case NotFound:
+		return http.StatusNotFound
+	case Timeout:
+		return http.StatusRequestTimeout
+	case UserAbort:
+		return http.StatusNoContent
+	case ConfigErr, InferErr, ServerErr:
+		fallthrough
+	default:
+		return http.StatusInternalServerError
+	}
+}
